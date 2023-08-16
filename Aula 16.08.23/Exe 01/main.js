dd85a39d8a0990ee1c232b1ebb7935b244b8ae0a
Eduardo_Degan <edegan@furb.br>
2023-08-16 16:11:23 -0300

    Entra21

---
+++ Aula 16.08.23/Exe 01/main.js
@@ -0,0 +1,11 @@
+var nome, idade;
+
+nome = prompt("Informe o seu nome ");
+idade = parseInt(prompt("Informe sua Idade"));
+
+document.write(`nome ${nome}`);
+document.write(`idade ${idade}`);
+
+document.write(typeof nome);
+document.write(typeof idade);
+

f779741342d18905c5a382348ae66dee7c67f8f3
Eduardo Degan <edegan@furb.br>
2023-10-05 02:34:39 -0300

    desafio bug gmt

--- src/typescript/Desafio/script.js
+++ src/typescript/Desafio/script.js
@@ -1,68 +1,65 @@
-// Referências de elemento
-var cpf = document.getElementById("cpf");
-var form = document.getElementById("cadastroForm");
-var dadosCadastradosDiv = document.getElementById("dadosCadastrados");
-// Array onde os dados vão ser salvos
+// Obtém referência no DOM
+var cpfInput = document.getElementById("cpf");
+var cadastroForm = document.getElementById("cadastroForm");
+var dadosCadastradosContainer = document.getElementById("dadosCadastrados");
+// Array para guardar os dados das vacinações cadastradas
 var dadosCadastrados = [];
-//  Calcular a nova data de retorno 30 dias após a data atual
-//  Pensei que fosse calcular a nova data de retorno 30 dias após a data de vacinação
-/*
-  function calcularDataRetorno(dataVacinacao: string): string {
-    const dataVacinacaoObj = new Date(dataVacinacao);
+// Função para calcular a data de retorno, 30 dias após a data da vacinação
+function calcularDataRetorno(dataVacinacao) {
+    // Bug 1 dia faltando por causa do fuso horário
+    // https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
+    var dataVacinacaoObj = new Date(dataVacinacao.replace(/-/g, "/"));
     dataVacinacaoObj.setDate(dataVacinacaoObj.getDate() + 30);
-   return dataVacinacaoObj.toLocaleDateString();
-  }
-*/
-function calcularDataRetorno() {
-    var dataVacinacaoObj = new Date();
-    dataVacinacaoObj.setDate(dataVacinacaoObj.getDate() + 30);
+    // Locale pt-BR
     return dataVacinacaoObj.toLocaleDateString();
 }
-// Apagar dado por index do array
+// Função para apagar uma vacinação do array de dados cadastrados
 function apagarVacina(index) {
     dadosCadastrados.splice(index, 1);
-    var dadosFormatados = dadosCadastrados.map(vacinaHTML).join("");
-    dadosCadastradosDiv.innerHTML = dadosFormatados;
+    // Formata os dados como HTML
+    var dadosFormatados = dadosCadastrados.map(vacinacaoHTML).join("");
+    // Atualiza o conteúdo no contêiner de dados cadastrados
+    dadosCadastradosContainer.innerHTML = dadosFormatados;
 }
-// Como os dados vão ser organizados no HTML
-function vacinaHTML(dados, index) {
-    // Formatar data
+// Função para formatar os dados da vacinação como HTML
+function vacinacaoHTML(dados, index) {
+    // Formatar a data no formato DD/MM/AAAA
     var dataFormatada = function (dateString) {
         var _a = dateString.split("-"), year = _a[0], month = _a[1], day = _a[2];
         return "".concat(day, "/").concat(month, "/").concat(year);
     };
-    return "\n  <div class=\"vacina-container\">\n    <div class=\"delete-button\" onclick=\"apagarVacina(".concat(index, ")\">\n      x\n    </div>\n    <div class=\"vacina-group\">\n    <strong>CPF:</strong> ").concat(dados.cpf, "<br />\n    <strong>Nome:</strong> ").concat(dados.nome, "<br />\n    <strong>Data de Nascimento:</strong> ").concat(dataFormatada(dados.dataNascimento), "<br />\n    <strong>Nome da Vacina:</strong> ").concat(dados.nomeVacina, "<br />\n    <strong>Data de Vacina\u00E7\u00E3o:</strong> ").concat(dataFormatada(dados.dataVacina), "<br />\n        <strong>Data de Retorno:</strong> ").concat(dados.returnData, "<br />\n      </div>\n    </div>\n  ");
+    return "\n    <div class=\"vacinacao-container\">\n      <div class=\"delete-button\" onclick=\"apagarVacina(".concat(index, ")\">\n        x\n      </div>\n      <div class=\"vacinacao-group\">\n        <strong>CPF:</strong> ").concat(dados.cpf, "<br />\n        <strong>Nome:</strong> ").concat(dados.nome, "<br />\n        <strong>Data de Nascimento:</strong> ").concat(dataFormatada(dados.dataNascimento), "<br />\n        <strong>Nome da Vacina:</strong> ").concat(dados.nomeVacina, "<br />\n        <strong>Data de Vacina\u00E7\u00E3o:</strong> ").concat(dataFormatada(dados.dataVacina), "<br />\n        <strong>Data de Retorno:</strong> ").concat(dados.dataRetorno, "<br />\n      </div>\n    </div>\n  ");
 }
-// Submit Event
-form.addEventListener("submit", function (event) {
+//Listener de evento para o formulário de cadastro
+cadastroForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    var formData = new FormData(form);
-    // Pega CPF
-    var cpf = formData.get("cpf");
-    // Alerts
-    if (cpf.length !== 14) {
+    var formData = new FormData(cadastroForm);
+    // Obtém o valor do CPF do formulário
+    var cpfFormData = formData.get("cpf");
+    // Valida se o CPF possui 11 caracteres
+    if (cpfFormData.length !== 14) {
         alert("CPF deve ter 11 caracteres.");
         return;
     }
-    // Informar valores
-    var formObject = {
+    // Cria um objeto de Vacinação com os dados do formulário
+    var dadosVacinacao = {
         cpf: formData.get("cpf"),
         nome: formData.get("nome"),
         dataNascimento: formData.get("dataNascimento"),
         nomeVacina: formData.get("nomeVacina"),
         dataVacina: formData.get("dataVacina"),
-        returnData: calcularDataRetorno(),
+        dataRetorno: calcularDataRetorno(formData.get("dataVacina")),
     };
-    // Insere os dados no array
-    dadosCadastrados.push(formObject);
-    // Formata dados do array como VacinaHTLM
-    var dadosFormatados = dadosCadastrados.map(vacinaHTML).join("");
-    // Manda para a Div
-    dadosCadastradosDiv.innerHTML = dadosFormatados;
+    // Adiciona os dados da vacinação ao array de dados cadastrados
+    dadosCadastrados.push(dadosVacinacao);
+    // Formata os dados como HTML e os exibe no contêiner de dados cadastrados
+    var dadosFormatados = dadosCadastrados.map(vacinacaoHTML).join("");
+    dadosCadastradosContainer.innerHTML = dadosFormatados;
 });
-// Formatar CPF
-cpf.addEventListener("input", function () {
-    var cpfFormatado = cpf.value.replace(/\D/g, "");
+// Listener de evento para formatar o input do CPF
+cpfInput.addEventListener("input", function () {
+    var cpfFormatado = cpfInput.value.replace(/\D/g, "");
     cpfFormatado = cpfFormatado.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-    cpf.value = cpfFormatado;
+    // Atualiza o valor do CPF com o CPF formatado
+    cpfInput.value = cpfFormatado;
 });
--- src/typescript/Desafio/script.ts
+++ src/typescript/Desafio/script.ts
@@ -1,105 +1,102 @@
-// Vacina
-interface Vacina {
+// Define a estrutura dos dados da vacinação
+interface Vacinacao {
   cpf: string;
   nome: string;
   dataNascimento: string;
   nomeVacina: string;
   dataVacina: string;
-  returnData: string;
+  dataRetorno: string;
 }
-
-// Referências de elemento
-const cpf = document.getElementById("cpf") as HTMLInputElement;
-const form = document.getElementById("cadastroForm") as HTMLFormElement;
-const dadosCadastradosDiv = document.getElementById(
+// Obtém referência no DOM
+const cpfInput = document.getElementById("cpf") as HTMLInputElement;
+const cadastroForm = document.getElementById("cadastroForm") as HTMLFormElement;
+const dadosCadastradosContainer = document.getElementById(
   "dadosCadastrados"
 ) as HTMLDivElement;
 
-// Array onde os dados vão ser salvos
-const dadosCadastrados: Vacina[] = [];
+// Array para guardar os dados das vacinações cadastradas
+const dadosCadastrados: Vacinacao[] = [];
 
-//  Calcular a nova data de retorno 30 dias após a data atual
-//  Pensei que fosse calcular a nova data de retorno 30 dias após a data de vacinação
-/*
-  function calcularDataRetorno(dataVacinacao: string): string {
-    const dataVacinacaoObj = new Date(dataVacinacao);
-    dataVacinacaoObj.setDate(dataVacinacaoObj.getDate() + 30); 
-   return dataVacinacaoObj.toLocaleDateString();
-  } 
-*/
-function calcularDataRetorno(): string {
-  const dataVacinacaoObj = new Date();
+// Função para calcular a data de retorno, 30 dias após a data da vacinação
+function calcularDataRetorno(dataVacinacao: string): string {
+  // Bug 1 dia faltando por causa do fuso horário
+  // https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
+  const dataVacinacaoObj = new Date(dataVacinacao.replace(/-/g, "/"));
   dataVacinacaoObj.setDate(dataVacinacaoObj.getDate() + 30);
+  // Locale pt-BR
   return dataVacinacaoObj.toLocaleDateString();
 }
 
-// Apagar dado por index do array
+// Função para apagar uma vacinação do array de dados cadastrados
 function apagarVacina(index: number): void {
   dadosCadastrados.splice(index, 1);
-  const dadosFormatados = dadosCadastrados.map(vacinaHTML).join("");
-  dadosCadastradosDiv.innerHTML = dadosFormatados;
+  // Formata os dados como HTML
+  const dadosFormatados = dadosCadastrados.map(vacinacaoHTML).join("");
+  // Atualiza o conteúdo no contêiner de dados cadastrados
+  dadosCadastradosContainer.innerHTML = dadosFormatados;
 }
-
-// Como os dados vão ser organizados no HTML
-function vacinaHTML(dados: Vacina, index: number): string {
-  // Formatar data
+// Função para formatar os dados da vacinação como HTML
+function vacinacaoHTML(dados: Vacinacao, index: number): string {
+  // Formatar a data no formato DD/MM/AAAA
   const dataFormatada = (dateString: string) => {
     const [year, month, day] = dateString.split("-");
     return `${day}/${month}/${year}`;
   };
   return `
-  <div class="vacina-container">
-    <div class="delete-button" onclick="apagarVacina(${index})">
-      x
-    </div>
-    <div class="vacina-group">
-    <strong>CPF:</strong> ${dados.cpf}<br />
-    <strong>Nome:</strong> ${dados.nome}<br />
-    <strong>Data de Nascimento:</strong> ${dataFormatada(
-      dados.dataNascimento
-    )}<br />
-    <strong>Nome da Vacina:</strong> ${dados.nomeVacina}<br />
-    <strong>Data de Vacinação:</strong> ${dataFormatada(dados.dataVacina)}<br />
-        <strong>Data de Retorno:</strong> ${dados.returnData}<br />
+    <div class="vacinacao-container">
+      <div class="delete-button" onclick="apagarVacina(${index})">
+        x
+      </div>
+      <div class="vacinacao-group">
+        <strong>CPF:</strong> ${dados.cpf}<br />
+        <strong>Nome:</strong> ${dados.nome}<br />
+        <strong>Data de Nascimento:</strong> ${dataFormatada(
+          dados.dataNascimento
+        )}<br />
+        <strong>Nome da Vacina:</strong> ${dados.nomeVacina}<br />
+        <strong>Data de Vacinação:</strong> ${dataFormatada(
+          dados.dataVacina
+        )}<br />
+        <strong>Data de Retorno:</strong> ${dados.dataRetorno}<br />
       </div>
     </div>
   `;
 }
 
-// Submit Event
-form.addEventListener("submit", function (event) {
+//Listener de evento para o formulário de cadastro
+cadastroForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  const formData = new FormData(form);
-  // Pega CPF
-  const cpf = formData.get("cpf") as string;
-  // Alerts
-  if (cpf.length !== 14) {
+  const formData = new FormData(cadastroForm);
+  // Obtém o valor do CPF do formulário
+  const cpfFormData = formData.get("cpf") as string;
+  // Valida se o CPF possui 11 caracteres
+  if (cpfFormData.length !== 14) {
     alert("CPF deve ter 11 caracteres.");
     return;
   }
-  // Informar valores
-  const formObject: Vacina = {
+  // Cria um objeto de Vacinação com os dados do formulário
+  const dadosVacinacao: Vacinacao = {
     cpf: formData.get("cpf") as string,
     nome: formData.get("nome") as string,
     dataNascimento: formData.get("dataNascimento") as string,
     nomeVacina: formData.get("nomeVacina") as string,
     dataVacina: formData.get("dataVacina") as string,
-    returnData: calcularDataRetorno(),
+    dataRetorno: calcularDataRetorno(formData.get("dataVacina") as string),
   };
-  // Insere os dados no array
-  dadosCadastrados.push(formObject);
-  // Formata dados do array como VacinaHTLM
-  const dadosFormatados = dadosCadastrados.map(vacinaHTML).join("");
-  // Manda para a Div
-  dadosCadastradosDiv.innerHTML = dadosFormatados;
+  // Adiciona os dados da vacinação ao array de dados cadastrados
+  dadosCadastrados.push(dadosVacinacao);
+  // Formata os dados como HTML e os exibe no contêiner de dados cadastrados
+  const dadosFormatados = dadosCadastrados.map(vacinacaoHTML).join("");
+  dadosCadastradosContainer.innerHTML = dadosFormatados;
 });
 
-// Formatar CPF
-cpf.addEventListener("input", function () {
-  let cpfFormatado = cpf.value.replace(/\D/g, "");
+// Listener de evento para formatar o input do CPF
+cpfInput.addEventListener("input", function () {
+  let cpfFormatado = cpfInput.value.replace(/\D/g, "");
   cpfFormatado = cpfFormatado.replace(
     /(\d{3})(\d{3})(\d{3})(\d{2})/,
     "$1.$2.$3-$4"
   );
-  cpf.value = cpfFormatado;
+  // Atualiza o valor do CPF com o CPF formatado
+  cpfInput.value = cpfFormatado;
 });
